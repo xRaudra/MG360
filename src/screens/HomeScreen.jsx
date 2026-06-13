54c465a6c6e3577d966cca2b30e3f06fb4651169
xRaudra <saurabh@sievanetworks2.com>
2026-06-13 23:45:45 +0530

    design: Quick Actions inner banner width 60px -> 72px

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -188,7 +188,7 @@ export default function HomeScreen({ onNavigate, isGuest = false }) {
                 {/* Icon banner */}
                 <div
                   className="w-full flex items-end justify-center flex-shrink-0"
-                  style={{ height: 52, width: 60, background: a.bannerBg, borderRadius: '0 0 10px 10px', paddingBottom: 8 }}
+                  style={{ height: 52, width: 72, background: a.bannerBg, borderRadius: '0 0 10px 10px', paddingBottom: 8 }}
                 >
                   {a.img && <img src={a.img} alt={a.label} style={{ height: 23, width: 'auto', objectFit: 'contain' }} />}
                 </div>

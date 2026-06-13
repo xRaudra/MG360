ecfe85adea0e5d4b25ef17b998da82b580b9e0b0
xRaudra <saurabh@sievanetworks2.com>
2026-06-13 21:53:41 +0530

    fix: Quick Actions icon size 22px -> 23px

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -161,7 +161,7 @@ export default function HomeScreen({ onNavigate, isGuest = false }) {
                   className="w-full flex items-end justify-center flex-shrink-0"
                   style={{ height: 52, background: a.bannerBg, borderRadius: '0 0 10px 10px', paddingBottom: 8 }}
                 >
-                  {a.img && <img src={a.img} alt={a.label} style={{ height: 22, width: 'auto', objectFit: 'contain' }} />}
+                  {a.img && <img src={a.img} alt={a.label} style={{ height: 23, width: 'auto', objectFit: 'contain' }} />}
                 </div>
                 {/* Label */}
                 <span
